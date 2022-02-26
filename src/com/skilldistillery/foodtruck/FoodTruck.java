3cceb8a0931a617454c736833ab9a13a6867500c
aslusaf <aslusaf@live.com>
2022-02-25 20:27:52 -0700

    Outlines complete

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,55 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	private static int cumulativeTruckID = 1000;
+
+	private String name;
+	private String foodType;
+	private double rating;
+	private int truckID;
+
+	public FoodTruck() {
+
+	}
+
+	public FoodTruck(String name, String foodType, double rating) {
+		setName(name);
+		setFoodType(foodType);
+		setRating(rating);
+		setTruckID();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public int getTruckID() {
+		return this.truckID;
+	}
+
+	public void setTruckID() {
+		this.truckID = cumulativeTruckID++;
+	}
+
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,10 +1,54 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int MAX_FOOD_TRUCKS = 5;
 
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+		FoodTruck ft = new FoodTruck();
+		FoodTruckApp fta = new FoodTruckApp();
+		FoodTruck[] ftArr = fta.collectFoodTruckInfo(kb, ft);
+		
+		//test code
+		for (int i=0; i < ftArr.length; i++) {
+			System.out.println(ftArr[i].getName() + ftArr[i].getFoodType() +ftArr[i].getRating() + ftArr[i].getTruckID());
+		
+		}
+		
+
+		kb.close();
+
+	}
+
+	public FoodTruck[] collectFoodTruckInfo(Scanner kb, FoodTruck ft) {
+		
+			int numOfFoodTrucks = 0;
+			FoodTruck[] ftArr = new FoodTruck[MAX_FOOD_TRUCKS];
+
+		do {
+			System.out.println("Enter the information for up to " + MAX_FOOD_TRUCKS + " food trucks below:");
+			System.out.print("\nEnter the name of the Food Truck: ");
+			String name = kb.next();
+			if (name.equalsIgnoreCase("quit")) {
+				break;
+			}
+			System.out.print("Enter the food type: ");
+			String foodType = kb.next();
+			System.out.println("Enter the rating: \n");
+			double rating = kb.nextDouble();
+			
+			
+			
+			ftArr[numOfFoodTrucks] = new FoodTruck(name, foodType, rating);
+			
+			numOfFoodTrucks++;
+		} while (numOfFoodTrucks < 5);
+		
+		return ftArr;
 	}
 
 }
